bd1fce8e760e7cd0226d26f262a7c4313f98cadb
kaniksena <74830809+kaniksena@users.noreply.github.com>
2024-01-28 19:27:29 +0300

    HomePage design update

--- src/components/ContentBox.js
+++ src/components/ContentBox.js
@@ -3,8 +3,9 @@ import './contentBox.css'
 function ContentBox({ title, content }) {
   return (
     <div className="content-box">
-      <h2>{title}</h2>
+      <h2>{title} </h2>
       <p>{content}</p>
+      <button type="submit" className="submit-button"  >Kolay Başvuru</button>
     </div>
   );
 }
