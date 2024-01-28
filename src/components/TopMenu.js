78816630703d9852efa28ce2972da96b9177aa08
kaniksena <74830809+kaniksena@users.noreply.github.com>
2024-01-28 19:42:36 +0300

    some folder location chages

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './topMenu.css';
-import './LoginPage.js'
+import '../pages/LoginPage.js'
 
 function TopMenu() {
   return (
