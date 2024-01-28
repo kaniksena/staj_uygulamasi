c0ed3f122379ac654f3bbaf88074371d4910635a
kaniksena <74830809+kaniksena@users.noreply.github.com>
2024-01-28 15:10:58 +0300

    İnitial Commit

---
+++ src/components/LoginPage.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './Login.css'; // CSS dosyasını import et
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Kullanıcı adı:', username);
+    console.log('Şifre:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-form">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Kullanıcı Adı:</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Şifre:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="submit-button">Giriş Yap</button>
+        </form>
+      </div>
+      
+      
+      <div className="circles-container">
+      <div className="quarter- circle">
+        
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
